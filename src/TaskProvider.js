a97d724c12fdb75bd65f1633936b8af44d39a836
Rafael Cintra <ra.cintra@gmail.com>
2020-08-12 14:22:30 -0300

    First Commit

---
+++ src/TaskProvider.js
@@ -0,0 +1,31 @@
+import React, { createContext, useState, useContext } from "react";
+import { v4 } from "uuid";
+
+const TaskContext = createContext();
+export const useTasks = () => useContext(TaskContext);
+
+export default function TaskProvider({ children }) {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) =>
+    setTasks([
+      ...tasks,
+      {
+        id: v4(),
+        task,
+        complete: false,
+      },
+    ]);
+
+  // 5
+  const setStatusTask = (id, status) => {
+    setTasks(tasks.map((t) => (t.id === id ? { ...t, complete: status } : t)));
+  };
+
+  // 6
+  return (
+    <TaskContext.Provider value={{ tasks, addTask, setStatusTask }}>
+      {children}
+    </TaskContext.Provider>
+  );
+}
